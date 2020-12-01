9fad83cd161f95bbbf6a75509dc85376cba0f655
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-01 12:31:44 +0300

    Maven support added.

--- src/main/java/com/compiled_with_no_errors/Main.java
+++ src/main/java/com/compiled_with_no_errors/Main.java
@@ -3,6 +3,6 @@ package com.compiled_with_no_errors;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        System.out.println("Welcome to the Mastering Java For Beginners");
     }
 }
