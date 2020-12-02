bfa78d7e4f7ca160502036a94886195761f5652a
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-02 15:25:28 +0300

    JavaDocs added

--- src/main/java/com/compiled_with_no_errors/tutorials/methods/Methods.java
+++ src/main/java/com/compiled_with_no_errors/tutorials/methods/Methods.java
@@ -2,14 +2,25 @@ package com.compiled_with_no_errors.tutorials.methods;
 
 public class Methods {
 
+    /**
+     * This method accepts no parameters and prints a single line.
+     */
     public void methodWithNoParameters(){
         System.out.println("This method, 'methodWithNoParameters', accepts no parameters and returns nothing.");
     }
 
+    /**
+     * This method prints its parameter.
+     * @param callerName This is a String input to be printed
+     */
     public void methodWithParameters(String callerName){
         System.out.println("This method, 'methodWithParameters', accepts the following parameter: " + callerName);
     }
 
+    /**
+     * This method will return a single value
+     * @return 1
+     */
     public int methodReturningInt(){
         int localInt = 1;
         System.out.println("This method, 'methodReturningInt', will return the following value: " + localInt);
