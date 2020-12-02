8a144082622671152452b36f9d12bfc7e9b6bac5
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-02 17:50:18 +0300

    Exercise - Addition

---
+++ src/main/java/com/compiled_with_no_errors/examples/MathematicalOperations.java
@@ -0,0 +1,17 @@
+package com.compiled_with_no_errors.examples;
+
+/**
+ * This class executes simple mathematical operations.
+ */
+public class MathematicalOperations {
+
+    /**
+     * This method adds given parameters and returns the result.
+     * @param x first parameter
+     * @param y second parameter
+     * @return result of x+y
+     */
+    public double addition(int x, int y){
+        return (double) x + (double) y;
+    }
+}
