87f393720601d4ede38047190bdcda1462724cc2
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-02 16:40:21 +0300

    Package examples.

---
+++ src/main/java/com/compiled_with_no_errors/tutorials/packages/floatings/Operations.java
@@ -0,0 +1,17 @@
+package com.compiled_with_no_errors.tutorials.packages.floatings;
+
+/**
+ * This class provides mathematical operations
+ */
+public class Operations {
+
+    /**
+     * This methods adds its parameters and returns the result.
+     * @param x first parameter
+     * @param y second parameter
+     * @return x+y
+     */
+    public float addition(float x, float y){
+        return x + y;
+    }
+}
