0efda465a10f20abb5e4bc735f6a8ba36f5d171a
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-02 17:14:12 +0300

    Revert "Extra files deleted"
    This reverts commit 17c4c9e6

---
+++ src/main/java/com/compiled_with_no_errors/Main.java
@@ -0,0 +1,8 @@
+package com.compiled_with_no_errors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Mastering Java For Beginners");
+    }
+}
