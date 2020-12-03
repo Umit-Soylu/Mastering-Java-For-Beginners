3c8686fbcbd174d7bf4e60ebd6bd2310886038b6
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-03 12:54:36 +0300

    Exercise - Multiplication

--- src/main/java/com/compiled_with_no_errors/Main.java
+++
@@ -1,8 +0,0 @@
-package com.compiled_with_no_errors;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Mastering Java For Beginners");
-    }
-}
---
+++ src/main/java/com/compiled_with_no_errors/exercises/mathematical_operations/MathematicalOperations.java
@@ -0,0 +1,17 @@
+package com.compiled_with_no_errors.exercises.mathematical_operations;
+
+/**
+ * This class implements mathematical operations for {@link Integer}.
+ */
+public class MathematicalOperations {
+
+    /**
+     * This methods computes multiplication of its parameters and returns the result.
+     * @param x first parameter
+     * @param y second parameter
+     * @return result of x * y
+     */
+    public long multiplication(int x, int y){
+        return (long) x * (long) y;
+    }
+}
