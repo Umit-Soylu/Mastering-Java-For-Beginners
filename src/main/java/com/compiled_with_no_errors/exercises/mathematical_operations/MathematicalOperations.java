c3e3d81a6c35a817b63c0e8bcbbb47b02a6f06e2
Umit-Soylu <umit_soylu@yahoo.com>
2020-12-21 14:06:06 +0300

    Division update

--- src/main/java/com/compiled_with_no_errors/exercises/mathematical_operations/MathematicalOperations.java
+++ src/main/java/com/compiled_with_no_errors/exercises/mathematical_operations/MathematicalOperations.java
@@ -14,6 +14,7 @@ public class MathematicalOperations {
     public long multiplication(int x, int y){
         return (long) x * (long) y;
     }
+    private static float delta = 10E-5F;
 
     /**
      * This methods computes division of its parameters and returns the result.
