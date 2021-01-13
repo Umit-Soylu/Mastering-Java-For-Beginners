8770227257761f9ee67993d5d641128a31cfcbb5
Umit-Soylu <umit_soylu@yahoo.com>
2021-01-13 14:35:41 +0300

    Update

--- src/main/java/com/compiled_with_no_errors/tutorials/enums/MyCarCollection.java
+++ src/main/java/com/compiled_with_no_errors/tutorials/enums/MyCarCollection.java
@@ -5,6 +5,22 @@ package com.compiled_with_no_errors.tutorials.enums;
  */
 public final class MyCarCollection {
     private final String brand;
+
+    // Here the list of Cars owned.
+    public static final MyCarCollection sport = new MyCarCollection("Porsche");
+    public static final MyCarCollection daily = new MyCarCollection("Jeep");
+    public static final MyCarCollection business = new MyCarCollection("BMW");
+
+    private static final MyCarCollection[] cars = new MyCarCollection[]{sport, daily, business};
+
+    /**
+     * This method returns list of Cars
+     * @return List of cars owned
+     */
+    public static MyCarCollection[] getCars() {
+        return cars;
+    }
+
     /**
      * A private constructor to prevent object creation.
      * @param brand Brand of the car
@@ -21,18 +37,5 @@ public final class MyCarCollection {
         return brand;
     }
 
-    // Here the list of Cars owned.
-    public static final MyCarCollection sport = new MyCarCollection("Porsche");
-    public static final MyCarCollection daily = new MyCarCollection("Jeep");
-    public static final MyCarCollection business = new MyCarCollection("BMW");
 
-    private static final MyCarCollection[] cars = new MyCarCollection[]{sport, daily, business};
-
-    /**
-     * This method returns list of Cars
-     * @return List of cars owned
-     */
-    public static MyCarCollection[] getCars() {
-        return cars;
-    }
 }
