dfd75aa7237c232c91e3d08e291d36ac0bdbffa3
Umit-Soylu <umit_soylu@yahoo.com>
2021-01-13 14:16:49 +0300

    Enum exercise

--- src/main/java/com/compiled_with_no_errors/exercises/project_phases/ProjectPhases.java
+++ src/main/java/com/compiled_with_no_errors/exercises/project_phases/ProjectPhases.java
@@ -14,17 +14,19 @@ public enum ProjectPhases {
     private static ProjectPhases currentPhase = ProjectPhases.initiate;
 
     /**
-     *
+     * This method returns the current phase of the project.
      * @return current phase of the project.
      */
-    public ProjectPhases getCurrentPhase() {
+    public static ProjectPhases getCurrentPhase() {
         return currentPhase;
     }
 
     /**
      * Process the project to the next phase.
+     * @implNote if the project is already in {@link ProjectPhases#closure}, will not do anything
      */
-    public void proceed(){
-
+    public static void proceed(){
+        if (currentPhase.ordinal() < ProjectPhases.values().length - 1)
+            currentPhase = ProjectPhases.values()[(currentPhase.ordinal() + 1)];
     }
 }
