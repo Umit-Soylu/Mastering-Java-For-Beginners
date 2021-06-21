993a7475b0824456e406647e64e9db1cc69aa34e
Umit-Soylu <umit_soylu@yahoo.com>
2021-06-21 15:01:13 +0300

    Project Phase-1

---
+++ src/main/java/com/compiled_with_no_errors/examples/simulation/SimulationCoordinator.java
@@ -0,0 +1,27 @@
+package com.compiled_with_no_errors.examples.simulation;
+
+import java.util.Scanner;
+
+/**
+ * This is the main class for executing simulation
+ */
+public class SimulationCoordinator {
+
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+
+        System.out.println("\n\n---This is the beginning of the simulation---\n\n");
+
+        System.out.println("\nPlease provide the number of turns the simulation will run:");
+        int turn = inputs.nextInt();
+
+        System.out.println("\nPlease provide the number of creatures:");
+        short numberOfCreatures = inputs.nextShort();
+
+        System.out.println("\nPlease provide the ratio of vegetation:");
+        float vegetationRatio = inputs.nextFloat();
+
+        System.out.println("\nStarting simulation for "+ turn + " turns with vegetation ratio of " + vegetationRatio + " and with " + numberOfCreatures + " creatures.");
+    }
+
+}
